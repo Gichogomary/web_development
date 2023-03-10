d7404ce6276c08012621b92fc3dfe31c01a7d73a
Gichogomary <marygichogo53@gmail.com>
2023-03-10 10:55:04 +0300

    javascript

---
+++ log in/javascript/index.js
@@ -0,0 +1,35 @@
+//create variables
+/*create variables*/
+let age = 16
+console.log(name)
+console.log(age)
+
+let name;
+console.log(name)
+let first_name="my"
+let second_name="name"
+let full_name=first_name+second_name
+console.log(full_name)
+
+
+
+let number =20
+if(number<10){
+    //codeif condition is true
+    console.log("number is less than ten")
+    }
+    let country = "chad"
+    let age = 18
+    if(country=="togo"){
+        //code is true
+        if (age>=18){
+            console.log("accepted")
+        
+
+        }
+
+    }else{
+        console.log("not accepted")
+    
+
+    }
